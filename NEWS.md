1a422cb80fd73dd708bc51979f06ac4953584d41
brendanf <brendan.furneaux@gmail.com>
2023-02-13 13:26:42 +0200

    refactor: use more uniform argument names, update docs using markdown

--- NEWS.md
+++ NEWS.md
@@ -2,14 +2,20 @@
 
 * Added a `NEWS.md` file to track changes to the package.
 * Fixed bug when some preclusters are empty.
-* **BREAKING CHANGE**: former functions `single_linkage_pool()` and
-  `single_linkage_matrix_thread()` are now combined in `single_linkage()`, with
-  the argument `method="tree"` for the 'pool' version, and `method="matrix"` for
-  the 'matrix_thread' version.
-* Instead of arguments `dmin`, `dmax`, and `dstep`, `single_linkage()` can now
-  alternatively take the argument `thresholds`, to explicitly define the desired
-  thresholds.  This is slightly slower in cases where the desired thresholds are
-  evenly spaced, but allows more flexibility.
+* **BREAKING CHANGE**: former functions `single_linkage_pool()`,
+  `single_linkage_multi()` and `single_linkage_matrix_thread()` are now combined
+  in `single_linkage()`, with the argument `method="tree"` for the 'pool' and
+  'multi' (with `which`) version, and `method="matrix"` for the 'matrix_thread'
+  version. The names of some arguments have also changed (e.g. `dmin` ->
+  `thresh_min`).
+* Instead of arguments `thresh_min`, `thresh_max`, and `thresh_step`,
+  `single_linkage()` can now alternatively take the argument `thresholds`, to
+  explicitly define the desired thresholds.  This is slightly slower in cases
+  where the desired thresholds are evenly spaced, but allows more flexibility
+  when they are not.
+* Added function `usearch_single_linkage()` which calculates a distance matrix
+  between DNA sequences using the external program usearch, and then clusters
+  the result with `single_linkage()`.
 
 # optimotu 0.1.0
 
--- R/single_linkage.R
+++ R/single_linkage.R
@@ -14,27 +14,27 @@ verify_which <- function(which, seqnames) {
    invisible(TRUE)
 }
 
-verify_threshold_steps <- function(dmin, dmax, dstep) {
-   if (is.null(dmin) || is.null(dmax) || is.null(dstep)) {
-      stop("either 'thresholds' or 'dmin', 'dmax', and 'dstep' must be given")
+verify_threshold_steps <- function(thresh_min, thresh_max, thresh_step) {
+   if (is.null(thresh_min) || is.null(thresh_max) || is.null(thresh_step)) {
+      stop("either 'thresholds' or 'thresh_min', 'thresh_max', and 'thresh_step' must be given")
    }
-   if (!is.numeric(dmin) || !is.numeric(dmax) || !is.numeric(dstep)) {
-      stop("'dmin', 'dmax', and 'dstep' must all be numbers.")
+   if (!is.numeric(thresh_min) || !is.numeric(thresh_max) || !is.numeric(thresh_step)) {
+      stop("'thresh_min', 'thresh_max', and 'thresh_step' must all be numbers.")
    }
-   if (length(dmin) != 1L || length(dmax) != 1L || length(dstep) != 1L) {
-      stop("'dmin', 'dmax', and 'dstep' must all be of length 1.")
+   if (length(thresh_min) != 1L || length(thresh_max) != 1L || length(thresh_step) != 1L) {
+      stop("'thresh_min', 'thresh_max', and 'thresh_step' must all be of length 1.")
    }
-   if (is.na(dmin) || is.na(dmax) || is.na(dstep)) {
-      stop("'dmin', 'dmax', and 'dstep' may not be NA.")
+   if (is.na(thresh_min) || is.na(thresh_max) || is.na(thresh_step)) {
+      stop("'thresh_min', 'thresh_max', and 'thresh_step' may not be NA.")
    }
-   if (is.nan(dmin) || is.nan(dmax) || is.nan(dstep)) {
-      stop("'dmin', 'dmax', and 'dstep' may not be NaN.")
+   if (is.nan(thresh_min) || is.nan(thresh_max) || is.nan(thresh_step)) {
+      stop("'thresh_min', 'thresh_max', and 'thresh_step' may not be NaN.")
    }
-   if (dstep <= 0) {
-      stop("'dstep' must be positive.")
+   if (thresh_step <= 0) {
+      stop("'thresh_step' must be positive.")
    }
-   if (dmax < dmin) {
-      stop("'dmax' must be greater than or equal to 'dmin'.")
+   if (thresh_max < thresh_min) {
+      stop("'thresh_max' must be greater than or equal to 'thresh_min'.")
    }
 }
 
@@ -82,32 +82,44 @@ verify_method_output_type <- function(
    invisible(TRUE)
 }
 
-#' Title
+#' Single linkage clustering at multiple thresholds from a sparse distance
+#' matrix
 #'
-#' @param file (`character` filename) The name of the file (or, e.g., a named
+#' @description This function is designed to reduce CPU and memory requirements
+#' of clustering by doing multiple clustering "jobs" concurrently from the same
+#' sparse distance matrix, which is read once and never kept in memory.  This is
+#' especially useful when the algorithm generating the distance matrix can also
+#' operate in a streaming fashion, so that distance matrices which are too large
+#' to fit in memory, or even to disk, can still be utilized. The motivating
+#' application is clustering biological sequences, but the implementation is
+#' totally agnostic about the nature of the entities it is clustering; it only
+#' needs distances.
+#'
+#' @param distmx (`character` filename) The name of the file (or, e.g., a named
 #' pipe) from which to read the sparse distance matrix. The matrix should be
-#' a whitespace delimited text file, with three values per line: seq1 seq2 dist,
-#' where seq1 and seq2 are numeric indices of the sequences to be clustered,
-#' starting with 0, and dist is the distance, typically a read number between 0
+#' a white-space delimited text file, with three values per line: id1 id2 dist,
+#' where id1 and id2 are integer indices of the objects to be clustered,
+#' starting with 0, and dist is the distance, typically a real number between 0
 #' and 1 (but the algorithm works for any non-negative distance.)
-#' @param seqnames (`character` vector) Names of the sequences, used for
+#' @param names (`character` vector) Names of the sequences, used for
 #' labeling the columns of the output matrix. Typically, in order to generate
 #' the sparse distance matrix with integer indices, an alternate version of the
-#' input file is written, where the sequence names are replaced with integers.
-#' These are the "real" names.
+#' input may be used, where the "real" names are replaced with integers. These
+#' are the "real" names.
 #' @param method (`character`) The algorithm to use; one of "tree" or "matrix".
 #' The "tree" algorithm is faster in at least some large cases, but tends to be
 #' slower in smaller cases, and cannot take advantage of parallel computation
 #' unless multiple overlapping subsets are specified in `which`. The two
 #' algorithms give identical results.
 #' @param output_type (`character`) Which type of output to give; one of
-#' "matrix" or "hclust". "matrix" returns an integer matrix giving clustering
-#' results, where the element in row `i` and column `j` give the 0-based index
-#' of the first member of the cluster to which sequence `j` belongs when
-#' clustered at the `i`th clustering threshold. "hclust" returns an object as
-#' returned by `stats::hclust()`, which requires less memory, especially for
-#' large problems, but is only supported for method "tree". If "which" is given,
-#' then either output_type returns a list whose elements are of the chosen type.
+#' `"matrix"` or `"hclust"`. `"matrix"` returns an integer matrix giving
+#' clustering results, where the element in row `i` and column `j` gives the
+#' 0-based index of the first member of the cluster to which sequence `j`
+#' belongs when clustered at the `i`th clustering threshold. `"hclust"` returns
+#' an object as returned by [stats::hclust()], which requires less memory,
+#' especially for large problems, but is only supported for method `"tree"`. If
+#' `which` is given, then either `output_type` returns a list whose elements are
+#' of the chosen type.
 #' @param thresholds (sorted `numeric` vector) An explicit list of clustering
 #' thresholds to try.  These do not need to be evenly spaced but must be
 #' strictly increasing.
@@ -116,40 +128,40 @@ verify_method_output_type <- function(
 #' `thresholds` are provided. If the actual precision of numbers in the distance
 #' matrix is smaller than this value, then distances will be rounded to this
 #' precision without warning.
+#' @param thresh_min (`numeric` scalar) The minimum distance threshold for
+#' clustering; should not be given if explicit `thresholds` are specified.
+#' @param thresh_max (`numeric` scalar) The maximum distance threshold for
+#' clustering; should not be given if explicit `thresholds` are specified.
+#' @param thresh_step (`numeric` scalar) The spacing between subsequent distance
+#' thresholds for clustering; should not be given if explicit `thresholds` are
+#' specified.
 #' @param which (`list` of `character` vectors) Instead of performing clustering
 #' on all input sequences, perform independent clustering on subsets of the
 #' sequences defined by the elements of `which`. Subsets do not need to be
 #' disjoint (and indeed, if they are it is probably faster to calculate the
 #' distance matrices separately.) Currently `which` is only implemented for the
-#' "tree" algorithm.
-#' @param dmin (`numeric` scalar) The minimum distance threshold for clustering;
-#' should not be given if explicit `thresholds` are specified.
-#' @param dmax (`numeric` scalar) The maximum distance threshold for clustering;
-#' should not be given if explicit `thresholds` are specified.
-#' @param dstep (`numeric` scalar) The spacing between subsequent distance
-#' thresholds for clustering; should not be given if explicit `thresholds` are
-#' specified.
+#' `"tree"` algorithm.
 #' @param threads (`integer` scalar) Maximum number of parallel threads.
 #' @param minsplit (`integer` scalar) Controls the granularity of parallel
 #' processing in the "matrix" algorithm.
 #'
 #' @return An [`integer matrix`][methods::StructureClasses] if
 #' `output_type=="matrix"`, an [`hclust`][stats::hclust] object if
-#' `output_type == "hclust"`, or a list of one of these when `which` is a list.
+#' `output_type=="hclust"`, or a list of one of these when `which` is a list.
 #' @export
 #'
 #' @examples
 single_linkage = function(
-   file,
-   seqnames,
+   distmx,
+   names,
    method = c("tree", "matrix"),
    output_type = c("matrix", "hclust"),
+   thresh_min = NULL,
+   thresh_max = NULL,
+   thresh_step = NULL,
    thresholds = NULL,
    precision = NULL,
    which = NULL,
-   dmin = NULL,
-   dmax = NULL,
-   dstep = NULL,
    threads = 1L,
    minsplit = 1L
 ) {
@@ -160,50 +172,78 @@ single_linkage = function(
       if (!is.null(precision)) {
          warning("'precision' has no effect when 'thresholds' is not given. Ignoring.\n")
       }
-      verify_threshold_steps(dmin, dmax, dstep)
+      verify_threshold_steps(thresh_min, thresh_max, thresh_step)
       if (!is.null(which) && !isTRUE(which)) {
-         verify_which(which, method, seqnames)
-         single_linkage_multi_uniform(file, seqnames, dmin, dmax, which, threads)
+         verify_which(which, method, names)
+         single_linkage_multi_uniform(
+            distmx,
+            names,
+            thresh_min,
+            thresh_max,
+            which,
+            threads
+         )
       } else {
          switch(
             method,
-            tree = single_linkage_pool_uniform(file, seqnames, dmin, dmax, dstep, output_type),
-            matrix = single_linkage_matrix_uniform(file, seqnames, dmin, dmax, dstep, threads, minsplit)
+            tree = single_linkage_pool_uniform(
+               distmx,
+               names,
+               thresh_min,
+               thresh_max,
+               thresh_step,
+               output_type
+            ),
+            matrix = single_linkage_matrix_uniform(
+               distmx,
+               names,
+               thresh_min,
+               thresh_max,
+               thresh_step,
+               threads,
+               minsplit
+            )
          )
       }
-   } else if (!is.null(dmin) || !is.null(dmax) || !is.null(dstep)) {
-      stop("If 'thresholds' is given, 'dmin', 'dmax', and 'dstep' must not be.")
+   } else if (!is.null(thresh_min) || !is.null(thresh_max) || !is.null(thresh_step)) {
+      stop("If 'thresholds' is given, 'thresh_min', 'thresh_max', and 'thresh_step' must not be.")
    } else {
       verify_thresholds(thresholds)
       verify_precision(precision)
       if (!is.null(which) && !isTRUE(which)) {
          verify_which(which, method, seqnames)
          if (is.null(precision)) {
-            single_linkage_multi_array(file, seqnames, thresholds, which, threads)
+            single_linkage_multi_array(distmx, names, thresholds, which, threads)
          } else {
-            single_linkage_multi_cached(file, seqnames, thresholds, precision, which, threads)
+            single_linkage_multi_cached(distmx, names, thresholds, precision, which, threads)
          }
       } else {
          if (is.null(precision)) {
             switch(
                method,
-               tree = single_linkage_pool_array(file, seqnames, thresholds, output_type),
-               matrix = single_linkage_matrix_array(file, seqnames, thresholds, threads, minsplit)
+               tree = single_linkage_pool_array(distmx, names, thresholds, output_type),
+               matrix = single_linkage_matrix_array(distmx, names, thresholds, threads, minsplit)
             )
          } else {
             switch(
                method,
-               tree = single_linkage_pool_cached(file, seqnames, thresholds, precision, output_type),
-               matrix = single_linkage_matrix_cached(file, seqnames, thresholds, precision, threads, minsplit)
+               tree = single_linkage_pool_cached(distmx, names, thresholds, precision, output_type),
+               matrix = single_linkage_matrix_cached(distmx, names, thresholds, precision, threads, minsplit)
             )
          }
       }
    }
 }
 
-#' Do single-linkage clustering at a series of increasing similarity thresholds.
+#' Do single-linkage clustering at a series of increasing similarity thresholds
+#' using USEARCH to calculate sequence similarities.
 #'
-#' @param seq (`character` vector, filename, or `Biostrings::DNAStringSet`)
+#' @description This function uses a unix pipe to direct the output of the
+#' USEARCH "`calc_distmx`" command  to [single_linkage()]. USEARCH (version 8.0
+#' or higher) should be installed separately; it is available with a free
+#' license for most users at [https://www.drive5.com/usearch/].
+#'
+#' @param seq (`character` vector, filename, or [DNAStringSet][Biostrings::DNAStringSet()])
 #' sequences to cluster
 #' @param seq_id (`character` vector) names for the sequences.  If they are
 #' already named, this will replace the names.  Has no effect if `seq` is a
@@ -419,9 +459,9 @@ do_usearch_singlelink <- function(
       out <- single_linkage(
          file = fifoname,
          seq_id,
-         dmin = thresh_min,
-         dmax = thresh_max,
-         dstep = thresh_step,
+         thresh_min = thresh_min,
+         thresh_max = thresh_max,
+         thresh_step = thresh_step,
          thresholds = thresholds,
          method = method,
          output_type = output_type,
@@ -433,9 +473,9 @@ do_usearch_singlelink <- function(
       out <- single_linkage(
          file = fifoname,
          seq_id,
-         dmin = thresh_min,
-         dmax = thresh_max,
-         dstep = thresh_step,
+         thresh_min = thresh_min,
+         thresh_max = thresh_max,
+         thresh_step = thresh_step,
          thresholds = thresholds,
          method = method,
          output_type = output_type,
