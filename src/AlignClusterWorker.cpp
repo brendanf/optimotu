fbea005577d138dbea891f216c3a597a20234645
brendanf <brendan.furneaux@gmail.com>
2023-05-22 15:23:04 +0300

    refactor: put class definitions in header

--- src/AlignClusterWorker.cpp
+++ src/AlignClusterWorker.cpp
@@ -1,217 +1,171 @@
-#include <Rcpp.h>
-#include <RcppParallel.h>
-#include <RcppThread.h>
-#include <bindings/cpp/WFAligner.hpp>
-#include <edlib.h>
-#include "pairwise_alignment.h"
-#include "ClusterAlgorithm.h"
-#include "ClusterMatrix.h"
-#include "ClusterIndexedMatrix.h"
-#include "ClusterTree.h"
+#include "AlignClusterWorker.h"
 
 typedef RcppParallel::RMatrix<int> matrix_t;
 
-class AlignClusterWorker : public RcppParallel::Worker {
-protected:
-  const std::vector<std::string> &seq;
-  const double breakpoint;
-  ClusterAlgorithm &clust_algo;
-  const uint8_t threads;
-  std::mutex mutex;
-  size_t _prealigned = 0, _aligned = 0;
-  const bool verbose;
-public :
-    AlignClusterWorker(
-      const std::vector<std::string> &seq,
-      const double breakpoint,
-      ClusterAlgorithm &clust_algo,
-      const uint8_t threads,
-      bool verbose = false
-    ) : seq(seq), breakpoint(breakpoint), clust_algo(clust_algo),
-    threads(threads), verbose(verbose) {};
+void HybridSplitClusterWorker::operator()(std::size_t begin, std::size_t end) {
+  double n = seq.size();
+  double m = (n*n - 3.0*n + 2.0)/2.0;
+  size_t my_prealigned = 0;
+  size_t my_aligned = 0;
+  size_t begin_i;
 
-    size_t prealigned() {
-      return _prealigned;
-    }
+  EdlibAlignConfig ed_aligner = edlibNewAlignConfig(-1, EdlibAlignMode::EDLIB_MODE_NW, EdlibAlignTask::EDLIB_TASK_PATH, 0, 0);
+  wfa::WFAlignerEdit wfa_aligner{wfa::WFAligner::Alignment};
+  ClusterAlgorithm * my_algo = clust_algo.make_child();
 
-    size_t aligned() {
-      return _aligned;
-    }
-};
-
-class HybridSplitClusterWorker : public AlignClusterWorker {
-public :
-  using AlignClusterWorker::AlignClusterWorker;
-  void operator()(std::size_t begin, std::size_t end) {
-    double n = seq.size();
-    double m = (n*n - 3.0*n + 2.0)/2.0;
-    size_t my_prealigned = 0;
-    size_t my_aligned = 0;
-    size_t begin_i;
-
-    EdlibAlignConfig ed_aligner = edlibNewAlignConfig(-1, EdlibAlignMode::EDLIB_MODE_NW, EdlibAlignTask::EDLIB_TASK_PATH, 0, 0);
-    wfa::WFAlignerEdit wfa_aligner{wfa::WFAligner::Alignment};
-    ClusterAlgorithm * my_algo = clust_algo.make_child();
-
-    if (begin == 0) {
-      begin_i = 1;
-    } else {
-      begin_i = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*begin)/threads - 1.0)));
-    }
-    size_t end_i   = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*end)/threads - 1.0)));
-    if (verbose) {
-      mutex.lock();
-      std::cout << "HybridSplit thread " << begin << " entered; sequences [" <<
-        begin_i << ", "<< end_i << ")" << std::endl;
-      mutex.unlock();
-    }
-    for (size_t i = begin_i; i < end_i; i++) {
-      for (size_t j = 0; j < i; j++) {
-        double threshold = my_algo->max_relevant(i, j);
-        // RcppThread::Rcout << "seqs " << j
-        //                   << " and " << i
-        //                   << " max relevant=" << threshold
-        //                   << std::endl;
-
-        bool is_seqj_longer = seq[j].size() > seq[i].size();
-        size_t s1 = is_seqj_longer ? i : j;
-        size_t s2 = is_seqj_longer ? j : i;
-        double l1 = seq[s1].size(), l2 = seq[s2].size();
-        // Rcpp::Rcout << "#### seq " << i << " (l1=" << l1 << ") and "
-        //             << j << " (l2=" << l2 <<")####" << std::endl;
-
-        double sim_threshold = 1.0 - threshold; // compiler can probably do this?
-        if (l1/l2 < sim_threshold) continue;
-        ++my_prealigned;
-        double sim_threshold_plus_1 = 2.0 - threshold;
-        double maxd1 = threshold * (l1 + l2) / sim_threshold_plus_1;
-        bool is_close = breakpoint >= 1 ? maxd1 < breakpoint : threshold < breakpoint;
-        double d;
-        if (is_close) {
-          int max_k = (int)ceil((l2 - l1 * sim_threshold) / sim_threshold_plus_1);
-          int min_k = -(int)ceil((l1 - l2 * sim_threshold) / sim_threshold_plus_1);
-          wfa_aligner.setHeuristicBandedStatic(min_k, max_k);
-          wfa_aligner.setMaxAlignmentScore((int)maxd1 + 1);
-          d = distance_wfa2(seq[s1], seq[s2], wfa_aligner);
-        } else {
-          ed_aligner.k = (int)maxd1 + 1;
-          d = distance_edlib(seq[s1], seq[s2], ed_aligner);
-        }
-        if (d < 1.0) ++my_aligned;
-
-        // std::cout << "distance=" << d
-        //                   << std::endl;
-        if (d < threshold) (*my_algo)(j, i, d);
-        RcppThread::checkUserInterrupt();
-      }
-    }
-    mutex.lock();
-    if (verbose) std::cout << "thread " << begin << " ready to merge" << std::endl;
-    _aligned += my_aligned;
-    _prealigned += my_prealigned;
-    mutex.unlock();
-    my_algo->merge_into_parent();
-    if (verbose) std::cout << "thread " << begin << " done" << std::endl;
+  if (begin == 0) {
+    begin_i = 1;
+  } else {
+    begin_i = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*begin)/threads - 1.0)));
   }
-};
-
-class HybridConcurrentClusterWorker : public AlignClusterWorker {
-public :
-  using AlignClusterWorker::AlignClusterWorker;
-
-  void operator()(std::size_t begin, std::size_t end) {
-    double n = seq.size();
-    double m = (n*n - 3.0*n + 2.0)/2.0;
-    size_t my_prealigned = 0;
-    size_t my_aligned = 0;
-    size_t begin_i;
-
-    EdlibAlignConfig ed_aligner = edlibNewAlignConfig(
-      -1,
-      EdlibAlignMode::EDLIB_MODE_NW,
-      EdlibAlignTask::EDLIB_TASK_PATH,
-      0,
-      0
-    );
-    wfa::WFAlignerEdit wfa_aligner{wfa::WFAligner::Alignment};
-
-    if (begin == 0) {
-      begin_i = 1;
-    } else {
-      begin_i = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*begin)/threads - 1.0)));
-    }
-    size_t end_i   = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*end)/threads - 1.0)));
-    if (verbose) {
-      mutex.lock();
-      std::cout << "HybridConcurrent thread " << begin << " entered; sequences [" <<
-        begin_i << ", "<< end_i << ")" << std::endl;
-      mutex.unlock();
-    }
-    for (size_t i = begin_i; i < end_i; i++) {
-      for (size_t j = 0; j < i; j++) {
-        // mutex.lock();
-        // std::cout << "Thread " << begin
-        //           << ": seqs " << j
-        //           << " and " << i
-        //           << std::endl;
-        // mutex.unlock();
-        double threshold = clust_algo.max_relevant(i, j);
-        // mutex.lock();
-        // std::cout << "Thread " << begin
-        //           << ": max relevant=" << threshold
-        //                   << std::endl;
-        // mutex.unlock();
-        bool is_seqj_longer = seq[j].size() > seq[i].size();
-        size_t s1 = is_seqj_longer ? i : j;
-        size_t s2 = is_seqj_longer ? j : i;
-        double l1 = seq[s1].size(), l2 = seq[s2].size();
-        // Rcpp::Rcout << "#### seq " << i << " (l1=" << l1 << ") and "
-        //             << j << " (l2=" << l2 <<")####" << std::endl;
-
-        double sim_threshold = 1.0 - threshold; // compiler can probably do this?
-        if (l1/l2 < sim_threshold) continue;
-        ++my_prealigned;
-        double sim_threshold_plus_1 = 2.0 - threshold;
-        double maxd1 = threshold * (l1 + l2) / sim_threshold_plus_1;
-        bool is_close = breakpoint >= 1 ? maxd1 < breakpoint : threshold < breakpoint;
-        double d;
-        if (is_close) {
-          int max_k = (int)ceil((l2 - l1 * sim_threshold) / sim_threshold_plus_1);
-          int min_k = -(int)ceil((l1 - l2 * sim_threshold) / sim_threshold_plus_1);
-          wfa_aligner.setHeuristicBandedStatic(min_k, max_k);
-          wfa_aligner.setMaxAlignmentScore((int)maxd1 + 1);
-          // std::cout << "wfa_aligner min_k=" << min_k
-          //           << " max_k=" << max_k
-          //           << " max score=" << (int)maxd1 + 1
-          //           << std::endl;
-          d = distance_wfa2(seq[s1], seq[s2], wfa_aligner);
-        } else {
-          ed_aligner.k = (int)maxd1 + 1;
-          d = distance_edlib(seq[s1], seq[s2], ed_aligner);
-        }
-        if (d < 1.0) ++my_aligned;
-        // mutex.lock();
-        // std::cout << "Thread " << begin
-        //           << ": distance=" << d
-        //           << std::endl;
-        // mutex.unlock();
-        if (d < threshold) clust_algo(j, i, d);
-        // mutex.lock();
-        // std::cout << "Thread " << begin
-        //           << ": finished " << j
-        //           << " and " << i
-        //           << std::endl;
-        // mutex.unlock();
-        RcppThread::checkUserInterrupt();
-      }
-    }
+  size_t end_i   = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*end)/threads - 1.0)));
+  if (verbose) {
     mutex.lock();
-    _aligned += my_aligned;
-    _prealigned += my_prealigned;
-    // std::cout << "Exiting thread " << begin << std::endl;
+    std::cout << "HybridSplit thread " << begin << " entered; sequences [" <<
+      begin_i << ", "<< end_i << ")" << std::endl;
     mutex.unlock();
   }
-};
+  for (size_t i = begin_i; i < end_i; i++) {
+    for (size_t j = 0; j < i; j++) {
+      double threshold = my_algo->max_relevant(i, j);
+      // RcppThread::Rcout << "seqs " << j
+      //                   << " and " << i
+      //                   << " max relevant=" << threshold
+      //                   << std::endl;
+
+      bool is_seqj_longer = seq[j].size() > seq[i].size();
+      size_t s1 = is_seqj_longer ? i : j;
+      size_t s2 = is_seqj_longer ? j : i;
+      double l1 = seq[s1].size(), l2 = seq[s2].size();
+      // Rcpp::Rcout << "#### seq " << i << " (l1=" << l1 << ") and "
+      //             << j << " (l2=" << l2 <<")####" << std::endl;
+
+      double sim_threshold = 1.0 - threshold; // compiler can probably do this?
+      if (l1/l2 < sim_threshold) continue;
+      ++my_prealigned;
+      double sim_threshold_plus_1 = 2.0 - threshold;
+      double maxd1 = threshold * (l1 + l2) / sim_threshold_plus_1;
+      bool is_close = breakpoint >= 1 ? maxd1 < breakpoint : threshold < breakpoint;
+      double d;
+      if (is_close) {
+        int max_k = (int)ceil((l2 - l1 * sim_threshold) / sim_threshold_plus_1);
+        int min_k = -(int)ceil((l1 - l2 * sim_threshold) / sim_threshold_plus_1);
+        wfa_aligner.setHeuristicBandedStatic(min_k, max_k);
+        wfa_aligner.setMaxAlignmentScore((int)maxd1 + 1);
+        d = distance_wfa2(seq[s1], seq[s2], wfa_aligner);
+      } else {
+        ed_aligner.k = (int)maxd1 + 1;
+        d = distance_edlib(seq[s1], seq[s2], ed_aligner);
+      }
+      if (d < 1.0) ++my_aligned;
+
+      // std::cout << "distance=" << d
+      //                   << std::endl;
+      if (d < threshold) (*my_algo)(j, i, d);
+      RcppThread::checkUserInterrupt();
+    }
+  }
+  mutex.lock();
+  if (verbose) std::cout << "thread " << begin << " ready to merge" << std::endl;
+  _aligned += my_aligned;
+  _prealigned += my_prealigned;
+  mutex.unlock();
+  my_algo->merge_into_parent();
+  if (verbose) std::cout << "thread " << begin << " done" << std::endl;
+}
+
+void HybridConcurrentClusterWorker::operator()(std::size_t begin, std::size_t end) {
+  double n = seq.size();
+  double m = (n*n - 3.0*n + 2.0)/2.0;
+  size_t my_prealigned = 0;
+  size_t my_aligned = 0;
+  size_t begin_i;
+
+  EdlibAlignConfig ed_aligner = edlibNewAlignConfig(
+    -1,
+    EdlibAlignMode::EDLIB_MODE_NW,
+    EdlibAlignTask::EDLIB_TASK_PATH,
+    0,
+    0
+  );
+  wfa::WFAlignerEdit wfa_aligner{wfa::WFAligner::Alignment};
+
+  if (begin == 0) {
+    begin_i = 1;
+  } else {
+    begin_i = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*begin)/threads - 1.0)));
+  }
+  size_t end_i   = round(1.5 + 0.5*sqrt(9.0 + 8.0*((m*end)/threads - 1.0)));
+  if (verbose) {
+    mutex.lock();
+    std::cout << "HybridConcurrent thread " << begin << " entered; sequences [" <<
+      begin_i << ", "<< end_i << ")" << std::endl;
+    mutex.unlock();
+  }
+  for (size_t i = begin_i; i < end_i; i++) {
+    for (size_t j = 0; j < i; j++) {
+      // mutex.lock();
+      // std::cout << "Thread " << begin
+      //           << ": seqs " << j
+      //           << " and " << i
+      //           << std::endl;
+      // mutex.unlock();
+      double threshold = clust_algo.max_relevant(i, j);
+      // mutex.lock();
+      // std::cout << "Thread " << begin
+      //           << ": max relevant=" << threshold
+      //                   << std::endl;
+      // mutex.unlock();
+      bool is_seqj_longer = seq[j].size() > seq[i].size();
+      size_t s1 = is_seqj_longer ? i : j;
+      size_t s2 = is_seqj_longer ? j : i;
+      double l1 = seq[s1].size(), l2 = seq[s2].size();
+      // Rcpp::Rcout << "#### seq " << i << " (l1=" << l1 << ") and "
+      //             << j << " (l2=" << l2 <<")####" << std::endl;
+
+      double sim_threshold = 1.0 - threshold; // compiler can probably do this?
+      if (l1/l2 < sim_threshold) continue;
+      ++my_prealigned;
+      double sim_threshold_plus_1 = 2.0 - threshold;
+      double maxd1 = threshold * (l1 + l2) / sim_threshold_plus_1;
+      bool is_close = breakpoint >= 1 ? maxd1 < breakpoint : threshold < breakpoint;
+      double d;
+      if (is_close) {
+        int max_k = (int)ceil((l2 - l1 * sim_threshold) / sim_threshold_plus_1);
+        int min_k = -(int)ceil((l1 - l2 * sim_threshold) / sim_threshold_plus_1);
+        wfa_aligner.setHeuristicBandedStatic(min_k, max_k);
+        wfa_aligner.setMaxAlignmentScore((int)maxd1 + 1);
+        // std::cout << "wfa_aligner min_k=" << min_k
+        //           << " max_k=" << max_k
+        //           << " max score=" << (int)maxd1 + 1
+        //           << std::endl;
+        d = distance_wfa2(seq[s1], seq[s2], wfa_aligner);
+      } else {
+        ed_aligner.k = (int)maxd1 + 1;
+        d = distance_edlib(seq[s1], seq[s2], ed_aligner);
+      }
+      if (d < 1.0) ++my_aligned;
+      // mutex.lock();
+      // std::cout << "Thread " << begin
+      //           << ": distance=" << d
+      //           << std::endl;
+      // mutex.unlock();
+      if (d < threshold) clust_algo(j, i, d);
+      // mutex.lock();
+      // std::cout << "Thread " << begin
+      //           << ": finished " << j
+      //           << " and " << i
+      //           << std::endl;
+      // mutex.unlock();
+      RcppThread::checkUserInterrupt();
+    }
+  }
+  mutex.lock();
+  _aligned += my_aligned;
+  _prealigned += my_prealigned;
+  // std::cout << "Exiting thread " << begin << std::endl;
+  mutex.unlock();
+}
 
 std::unique_ptr<SingleClusterAlgorithm> create_cluster_algorithm(
     const std::string &method,
---
+++ src/AlignClusterWorker.h
@@ -0,0 +1,54 @@
+#ifndef _AlignClusterWorker_
+#define _AlignClusterWorker_
+#include <Rcpp.h>
+#include <RcppParallel.h>
+#include <RcppThread.h>
+#include <bindings/cpp/WFAligner.hpp>
+#include <edlib.h>
+#include "pairwise_alignment.h"
+#include "ClusterAlgorithm.h"
+#include "ClusterMatrix.h"
+#include "ClusterIndexedMatrix.h"
+#include "ClusterTree.h"
+
+class AlignClusterWorker : public RcppParallel::Worker {
+protected:
+  const std::vector<std::string> &seq;
+  const double breakpoint;
+  ClusterAlgorithm &clust_algo;
+  const uint8_t threads;
+  std::mutex mutex;
+  size_t _prealigned = 0, _aligned = 0;
+  const bool verbose;
+public :
+  AlignClusterWorker(
+    const std::vector<std::string> &seq,
+    const double breakpoint,
+    ClusterAlgorithm &clust_algo,
+    const uint8_t threads,
+    bool verbose = false
+  ) : seq(seq), breakpoint(breakpoint), clust_algo(clust_algo),
+  threads(threads), verbose(verbose) {};
+
+  size_t prealigned() {
+    return _prealigned;
+  }
+
+  size_t aligned() {
+    return _aligned;
+  }
+};
+
+class HybridSplitClusterWorker : public AlignClusterWorker {
+public :
+  using AlignClusterWorker::AlignClusterWorker;
+  void operator()(std::size_t begin, std::size_t end);
+};
+
+class HybridConcurrentClusterWorker : public AlignClusterWorker {
+public :
+  using AlignClusterWorker::AlignClusterWorker;
+  void operator()(std::size_t begin, std::size_t end);
+};
+
+#endif
