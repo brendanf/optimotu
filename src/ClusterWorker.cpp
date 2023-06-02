b51308fdc42ae7f10be11dfcd9162179068b01e9
brendanf <brendan.furneaux@gmail.com>
2023-06-02 16:08:14 +0300

    remove: debugging output

--- src/ClusterWorker.cpp
+++ src/ClusterWorker.cpp
@@ -6,21 +6,25 @@ int ClusterWorker::n_threads() {
   return this->threads;
 }
 
-MergeClusterWorker::MergeClusterWorker(ClusterAlgorithm *algo, std::istream &file, const int threads) :
+MergeClusterWorker::MergeClusterWorker(
+  ClusterAlgorithm *algo,
+  std::istream &file,
+  const int threads
+) :
   ClusterWorker(file, threads),
   algo_list(threads) {
-  std::cout << "MergeClusterWorker constructor start...";
+  // std::cout << "MergeClusterWorker constructor start...";
   for (int i = 0; i < threads; ++i) {
     algo_list[i] = algo->make_child();
   }
-  std::cout << "done" << std::endl;
+  // std::cout << "done" << std::endl;
 };
 
 void MergeClusterWorker::operator()(size_t begin, size_t end) {
   DistanceElement d;
   std::vector<DistanceElement> buffer;
   buffer.reserve(100);
-  std::cout << "Starting ClusterWorker thread " << begin << std::endl;
+  // std::cout << "Starting MergeClusterWorker thread " << begin << std::endl;
   while (true) {
     mutex.lock();
     for (int i = 0; i < 100 && file; ++i) {
@@ -34,14 +38,14 @@ void MergeClusterWorker::operator()(size_t begin, size_t end) {
     }
     buffer.clear();
   }
-  mutex.lock();
-  std::cout << "ClusterWorker thread " << begin << " merging..." << std::endl;
-  mutex.unlock();
+  // mutex.lock();
+  // std::cout << "ClusterWorker thread " << begin << " merging..." << std::endl;
+  // mutex.unlock();
   algo_list[begin]->merge_into_parent();
 
-  mutex.lock();
-  std::cout << "ClusterWorker thread " << begin << " exiting" << std::endl;
-  mutex.unlock();
+  // mutex.lock();
+  // std::cout << "ClusterWorker thread " << begin << " exiting" << std::endl;
+  // mutex.unlock();
 }
 
 void MergeClusterWorker::finalize() {};
@@ -58,13 +62,15 @@ void ConcurrentClusterWorker::operator()(size_t begin, size_t end) {
   DistanceElement d;
   std::vector<DistanceElement> buffer;
   buffer.reserve(100);
-  std::cout << "Starting ConcurrentClusterWorker thread " << begin << std::endl;
+  // std::cout << "Starting ConcurrentClusterWorker thread " << begin << std::endl;
   while (true) {
     mutex.lock();
+    // std::cout << "buffering..." << std::flush;
     for (int i = 0; i < 100 && file; ++i) {
       file >> d;
       buffer.push_back(d);
     }
+    // std::cout << "done" << std::endl;
     mutex.unlock();
     if (buffer.size() == 0) break;
     for (auto d : buffer) {
@@ -73,9 +79,9 @@ void ConcurrentClusterWorker::operator()(size_t begin, size_t end) {
     buffer.clear();
   }
 
-  mutex.lock();
-  std::cout << "ConcurrentClusterWorker thread " << begin << " exiting" << std::endl;
-  mutex.unlock();
+  // mutex.lock();
+  // std::cout << "ConcurrentClusterWorker thread " << begin << " exiting" << std::endl;
+  // mutex.unlock();
 }
 
 void ConcurrentClusterWorker::finalize() {};
@@ -87,11 +93,11 @@ HierarchicalClusterWorker::HierarchicalClusterWorker(
   const int shards
 ) : ClusterWorker(file, threads), algo_list(shards), shards(shards),
 thread_count(shards) {
-  std::cout << "HieararchicalClusterWorker constructor start...";
+  // std::cout << "HieararchicalClusterWorker constructor start...";
   for (int i = 0; i < shards; ++i) {
     algo_list[i] = algo->make_child();
   }
-  std::cout << "done" << std::endl;
+  // std::cout << "done" << std::endl;
 }
 
 void HierarchicalClusterWorker::operator()(size_t begin, size_t end) {
@@ -99,14 +105,15 @@ void HierarchicalClusterWorker::operator()(size_t begin, size_t end) {
   std::vector<DistanceElement> buffer;
   buffer.reserve(100);
   size_t i = begin % shards;
-  mutex.lock();
-  ++thread_count[i];
-  std::cout << "Starting HierarchicalClusterWorker thread " << begin
-            << " (shard " << i
-            << " with " << thread_count[i]
-            << " previous threads)"
-            << std::endl;
-  mutex.unlock();
+  // mutex.lock();
+  // size_t tc =
+    ++thread_count[i];
+  // std::cout << "Starting HierarchicalClusterWorker thread " << begin
+  //           << " (shard " << i
+  //           << " with " << tc
+  //           << " previous threads)"
+  //           << std::endl;
+  // mutex.unlock();
   while (true) {
     mutex.lock();
     for (int i = 0; i < 100 && file; ++i) {
@@ -121,18 +128,18 @@ void HierarchicalClusterWorker::operator()(size_t begin, size_t end) {
     buffer.clear();
   }
   if (--thread_count[i] == 0) {
-    mutex.lock();
-    std::cout << "HierarchicalClusterWorker thread " << begin
-              << " merging shard " << i
-              << "..." << std::endl;
-    mutex.unlock();
+    // mutex.lock();
+    // std::cout << "HierarchicalClusterWorker thread " << begin
+    //           << " merging shard " << i
+    //           << "..." << std::endl;
+    // mutex.unlock();
     algo_list[i]->merge_into_parent();
   }
 
-    mutex.lock();
-    std::cout << "HieararchicalClusterWorker thread " << begin
-              << " exiting" << std::endl;
-    mutex.unlock();
+    // mutex.lock();
+    // std::cout << "HieararchicalClusterWorker thread " << begin
+    //           << " exiting" << std::endl;
+    // mutex.unlock();
   }
 
   void HierarchicalClusterWorker::finalize() {
